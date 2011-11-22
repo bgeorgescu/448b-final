0a5c571cfdc23e02adb72cdb41d4d37c8ab893ec
T.J. Purtell <tpurtell@stanford.edu>
2011-11-21 21:48:06 -0800

    mysql output, not tested, but compiles, dies waiting for connection.

--- eclipse/data/src/main/java/vis/data/LoadXML.java
+++ eclipse/data/src/main/java/vis/data/LoadXML.java
@@ -1,9 +1,17 @@
 package vis.data;
 
 import java.io.File;
+import java.lang.reflect.Array;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.TreeMap;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
@@ -44,10 +52,11 @@ public class LoadXML {
 				
 				while(!directories_to_scan.isEmpty()) {
 					File dir = directories_to_scan.poll();
+					System.out.println ("Scanning " + dir);
 					File[] files = dir.listFiles();
 					for(File file : files) {
 						if(file.isDirectory()) {
-							directories_to_scan.add(file);
+							directories_to_scan.addFirst(file);
 							continue;
 						} 
 						String name = file.getName().toLowerCase();
@@ -67,9 +76,10 @@ public class LoadXML {
 		
 		
 		//threads to process individual files
-		Thread processing_threads[];
-		processing_threads = new Thread[Runtime.getRuntime().availableProcessors()];
+		final Thread processing_threads[] = new Thread[Runtime.getRuntime().availableProcessors()];
 		
+		
+		final BlockingQueue<TreeMap<String, String>> documents_to_process = new ArrayBlockingQueue<TreeMap<String, String>>(1000);
 		final XPathFactory xpf = XPathFactory.newInstance();
 		final DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 		dbf.setNamespaceAware(true); // never forget this!
@@ -111,6 +121,7 @@ public class LoadXML {
 						} catch (InterruptedException e) {
 							throw new RuntimeException("Unknown interupt while inserting into file queue", e);
 						}
+						System.out.println ("Processing " + f);
 						Document doc;
 						try {
 							 doc = db.parse(f);
@@ -149,13 +160,100 @@ public class LoadXML {
 							System.err.println("Failed to run xpath query on " + f + " " + e.getMessage() + ":");
 							e.printStackTrace(System.err);
 						}
-						System.out.println(data.get("title"));
-						//TODO: something useful
+						try {
+							documents_to_process.put(data);
+						} catch (InterruptedException e) {
+							throw new RuntimeException("Unexpected failure inserting document to upload", e);
+						}
 					}
 				}
 			});
 			processing_threads[i].start();
 		}
+		final int BATCH_SIZE = 100;
+		final String TABLE_NAME = "rawdoc";
+		Thread mysql_thread = new Thread(new Runnable() {
+			public void run() {
+				Connection conn = null;
+
+				try
+				{
+					System.out.println ("Trying to connect to database");
+					String userName = "testuser";
+					String password = "testpass";
+					String url = "jdbc:mysql://localhost/test";
+					Class.forName ("com.mysql.jdbc.Driver").newInstance ();
+					conn = DriverManager.getConnection (url, userName, password);
+					if(conn == null)
+						throw new RuntimeException("unknown sql connection creation returned null");
+					System.out.println ("Database connection established");
+				}
+				catch (Exception e)
+				{
+					System.err.println ("Cannot connect to database server");
+					throw new RuntimeException("Sql connection failed", e);
+				}
+				int current_batch = 0;
+				PreparedStatement insert = null;
+				try {
+					for(;;) {
+						if(documents_to_process.isEmpty()) {
+							boolean still_running = false;
+							for(int i = 0; i < processing_threads.length; ++i)
+								still_running |= processing_threads[i].isAlive();
+							if(!still_running)
+								break;
+						}
+						TreeMap<String, String> data;
+						try {
+							data = documents_to_process.poll(5, TimeUnit.SECONDS);
+							//maybe we are out of work
+							if(data == null)
+								continue;
+						} catch (InterruptedException e) {
+							throw new RuntimeException("Unknown interupt while inserting into mysql queue", e);
+						}
+						//lazily create this so we don't have to have a copy of the field list in multiple places
+						if(insert == null) {
+							StringBuilder questions = new StringBuilder("?");
+							Iterator<String> j = data.keySet().iterator();
+							StringBuilder parameters = new StringBuilder(j.next());
+							for(int i = 1; i < data.size(); ++i) {
+								questions.append(", ?");
+								parameters.append(", " + j.next());
+							}
+							insert = conn.prepareStatement("INSERT INTO " + TABLE_NAME + "VALUES(" + questions + ")");
+						}
+						
+						Iterator<String> j = data.values().iterator();
+						int param_count = data.size();
+						for(int i = 0; i < param_count; ++i) {
+							insert.setString(i, j.next());
+						}
+						insert.addBatch();
+
+						if(++current_batch == BATCH_SIZE) {
+							System.out.println ("Inserting Batch " + current_batch);
+							insert.executeBatch();
+							current_batch = 0;
+						}
+						
+					}
+				} catch (SQLException e) {
+					throw new RuntimeException("failed to insert documents", e);
+				}
+				finally
+				{
+					try {
+						conn.close();
+						System.out.println ("Database connection terminated");
+					} catch (SQLException e) {
+						throw new RuntimeException("Database connection terminated funny", e);
+					}
+				}
+			}
+		});
+		mysql_thread.start();
 		
 		//join on them all
 		
