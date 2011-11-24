73a21853aa8bcfed045f8c4d83333b2617b4a13c
T.J. Purtell <tpurtell@stanford.edu>
2011-11-24 00:26:23 -0800

    more idlist utils

--- eclipse/data/src/main/java/vis/data/model/meta/IdLists.java
+++ eclipse/data/src/main/java/vis/data/model/meta/IdLists.java
@@ -5,6 +5,9 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 import vis.data.model.RawDoc;
+import vis.data.model.RawEntity;
+import vis.data.model.RawLemma;
+import vis.data.model.RawWord;
 
 public class IdLists {
 	public static int[] allDocs(Connection conn) {
@@ -35,5 +38,88 @@ public class IdLists {
 			throw new RuntimeException("failed to load doc list", e);
 		}
 	}
-
+	public static int[] allLemmas(Connection conn) {
+		int[] doc_ids;
+		try {
+			Statement st = conn.createStatement();
+			ResultSet rs = st.executeQuery("SELECT COUNT(*) FROM " + RawLemma.TABLE);
+			int doc_count;
+			try {
+				if(!rs.next())
+					throw new RuntimeException("fatal sql mistake");
+				doc_count = rs.getInt(1);
+			} finally {
+				rs.close();
+			}
+			doc_ids = new int[doc_count];
+			rs = st.executeQuery("SELECT " + RawLemma.ID + " FROM " + RawLemma.TABLE);
+			try {
+				int i = 0;
+				while(rs.next()) {
+					doc_ids[i++] = rs.getInt(1);
+				}
+			} finally {
+				rs.close();
+			}
+			return doc_ids;
+		} catch(Exception e) {
+			throw new RuntimeException("failed to load lemma list", e);
+		}
+	}
+	public static int[] allWords(Connection conn) {
+		int[] doc_ids;
+		try {
+			Statement st = conn.createStatement();
+			ResultSet rs = st.executeQuery("SELECT COUNT(*) FROM " + RawWord.TABLE);
+			int doc_count;
+			try {
+				if(!rs.next())
+					throw new RuntimeException("fatal sql mistake");
+				doc_count = rs.getInt(1);
+			} finally {
+				rs.close();
+			}
+			doc_ids = new int[doc_count];
+			rs = st.executeQuery("SELECT " + RawWord.ID + " FROM " + RawWord.TABLE);
+			try {
+				int i = 0;
+				while(rs.next()) {
+					doc_ids[i++] = rs.getInt(1);
+				}
+			} finally {
+				rs.close();
+			}
+			return doc_ids;
+		} catch(Exception e) {
+			throw new RuntimeException("failed to load word list", e);
+		}
+	}
+	public static int[] allEntities(Connection conn) {
+		int[] doc_ids;
+		try {
+			Statement st = conn.createStatement();
+			ResultSet rs = st.executeQuery("SELECT COUNT(*) FROM " + RawEntity.TABLE);
+			int doc_count;
+			try {
+				if(!rs.next())
+					throw new RuntimeException("fatal sql mistake");
+				doc_count = rs.getInt(1);
+			} finally {
+				rs.close();
+			}
+			doc_ids = new int[doc_count];
+			rs = st.executeQuery("SELECT " + RawEntity.ID + " FROM " + RawEntity.TABLE);
+			try {
+				int i = 0;
+				while(rs.next()) {
+					doc_ids[i++] = rs.getInt(1);
+				}
+			} finally {
+				rs.close();
+			}
+			return doc_ids;
+		} catch(Exception e) {
+			throw new RuntimeException("failed to load entity list", e);
+		}
+	}
 }
