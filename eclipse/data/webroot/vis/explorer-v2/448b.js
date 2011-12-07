b06a2b9ac11a347a7e02f9ceb2ffc27b5559aee1
Bobby Georgescu <bgeorgescu@gmail.com>
2011-12-07 13:31:08 -0800

    Query generation support

--- eclipse/data/webroot/vis/explorer-v2/448b.js
+++ eclipse/data/webroot/vis/explorer-v2/448b.js
@@ -135,7 +135,11 @@ function domStateToObject() {
 							});
 						})
 				}
-			})
+			}),
+		startYear: 2000,
+		endYear: 2010,
+		horizontalAxis: "date",
+		dateGranularity: "month"
 	}
 }
 
@@ -158,10 +162,189 @@ function objectToDomState(obj) {
 	});
 }
 
+function array_range(x,y,step) {
+	var retval = [];
+	if(!step) step = 1;
+	for(var i = x; i <= y; i+=step)
+		retval.push(i);
+	return retval;
+}
+
+
+/*
+
+snippet to find first literal of plottable series (to be used for legend):
+$(".series").filter(function(x) { return $(this).find(".literal").length; }).find(".literal").first();
+
+*/
+
+function queryForObject(state) {
+	var query = {};
+	var AndHelper = function(arr) { return arr.length > 1 ? {and_:{terms_: arr }} : arr[0]; };
+	var OrHelper = function(arr) { return arr.length > 1 ? {or_:{terms_: arr }} : arr[0]; };
+	var WordToTerm = LemmaTerm;
+	
+	query.filter_ = {date_:{before_:(state.endYear+1)*10000, after_:(state.startYear*10000 - 1)}};
+	
+	query.series_ = state.series
+		.filter(function(x) {
+			return x.contents.length
+		})
+		.map(function(x) {
+			return AndHelper(x.contents
+				.filter(function(y) {
+					return y.length;
+				})
+				.map(function(y) {
+					return OrHelper(y.map(function(z) {
+							return WordToTerm(z.text);
+						}));
+				}));
+		}).filter(function(x) {
+			return x;
+		});
+	
+	if (state.horizontalAxis == "page") {
+		query.buckets_ = array_range(1, 30).map(function(a) {
+			return PageTerm(a,a+1);
+		});
+	} else if(state.dateGranularity == "year") {
+		query.buckets_ = array_range(state.startYear, state.endYear).map(function(a) {
+			return YearTerm(a);
+		});
+	} else if (state.dateGranularity == "month") {
+		query.buckets_ = array_range(state.startYear, state.endYear).map(function(a) {
+			return array_range(0,11).map(function(b) {
+				return MonthTerm(a,b);
+			});
+		}).reduce(function(m,n) {
+			return m.concat(n);
+		});
+	} 
+	
+	return query;
+}
+
+
+var viewModel = {};
+
+viewModel.horizontalAxis = function() { return "date" };
+viewModel.dateGranularity = function() { return "month" };
+viewModel.graphStack = function() { return true; };
+viewModel.graphFill = function() { return true; };
+viewModel.graphMode = function() { return "bars"; };
+viewModel.startYear = function() { return 2000; };
+viewModel.endYear = function() { return 2010; };
+
+
+viewModel.graphData = function(data) {
+	if(data) {
+		viewModel.graphData_ = data;
+		updatePlot();
+	}
+	else {
+		return viewModel.graphData_;
+	}
+};
 
 
 
+var current_generation = 0;
+function queryChanged() {
+	// will get called anytime the query gets changed in any way
+	// we probably want to do some rate-limiting to avoid DoSing the server with queries
+    var query = queryForObject(domStateToObject());
+    arbitraryQuery("/api/query/docs/bucketed",    
+        query,
+        function(gen,query,c,r,d){
+            if(!success(c)) {
+                alert("query failed to run: " + r);
+                alert(JSON.stringify(query));
+                return;
+            }
+            if(gen != current_generation)
+                return;
+            //TODO: maybe a better way to do this
+            if(viewModel.horizontalAxis() == "page") {
+                viewModel.graphData(
+                    r
+                    .map(function(x, x_i) {
+                        
+                        return {data: x.map(function(y,y_i) {
+                            return [y_i + 1,y];
+                        }), label: "" };
+                    }));
+            } 
+            else if(viewModel.dateGranularity() == "year") {
+				viewModel.graphData(
+					r
+					.map(function(x, x_i) {
+						
+						return {data: x.map(function(y,y_i) {
+							return [new Date(viewModel.startYear()+y_i,0,0).getTime(),y];
+						}), label: "" };
+					}));
+            } else if(viewModel.dateGranularity() == "month") {
+				viewModel.graphData(
+					r
+					.map(function(x, x_i) {
+						
+						return {data: x.map(function(y,y_i) {
+							return [new Date(viewModel.startYear(),y_i,0).getTime(),y];
+						}), label: "" };
+					}));
+            }
+        }.bind(this, ++current_generation, query));
+}
 
+viewModel.graphOptions = function() {
+    var retval = {
+		series: {
+			stack: viewModel.graphStack() ? 1 : null,
+			lines: { show: (viewModel.graphMode() == "lines" || viewModel.graphMode() == "steps"), fill: viewModel.graphFill(), steps: viewModel.graphMode() == "steps" },
+			bars: { show: viewModel.graphMode() == "bars",
+					barWidth: 0.8 }
+		},
+		xaxis: {},
+		yaxis: {min: 0 },
+    };
+    
+    if(viewModel.horizontalAxis() == "page") {
+    	retval.xaxis.mode = null;
+		retval.xaxis.min = 0;
+		retval.xaxis.max = 32;
+    }
+    else if(viewModel.horizontalAxis() == "date") {
+    	retval.xaxis.mode = "time";
+		retval.xaxis.min = new Date(viewModel.startYear(), 0, 1).getTime();
+		if(viewModel.graphMode() == "lines" || viewModel.graphMode() == "steps") {
+			if(viewModel.dateGranularity() == "year") {
+				retval.xaxis.max = new Date(viewModel.endYear(), 0, 1).getTime();
+			} else if(viewModel.dateGranularity() == "month") {
+				retval.xaxis.max = new Date(viewModel.endYear(), 11, 0).getTime();
+			}
+		}
+		else {
+			retval.xaxis.max = new Date(viewModel.endYear(), 11, 30).getTime();
+			if(viewModel.dateGranularity() == "year") {
+				retval.series.bars.barWidth = 0.8*365*86400000;	
+			} else if(viewModel.dateGranularity() == "month") {
+				retval.series.bars.barWidth = 0.8*28*86400000;
+			}
+		}
+		if(viewModel.dateGranularity() == "year") {
+			retval.xaxis.minTickWidth = [1, "year"];
+
+		} else if(viewModel.dateGranularity() == "month") {
+			retval.xaxis.minTickWidth = [1, "month"];
+		}
+    }
+    return retval;
+}
+
+function updatePlot() {
+	$.plot($("#plot"), viewModel.graphData(), viewModel.graphOptions());
+}
 
 s1 = Series();
 s2 = Series();
